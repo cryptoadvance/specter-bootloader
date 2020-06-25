135ebd547e895ec2c1b6868729a6c32633053155
Mike Tolkachev <mstolkachev@gmail.com>
2020-06-25 22:47:09 +0300

    Add subcommands to upgreade-generator

--- tools/upgrade-generator.py
+++ tools/upgrade-generator.py
@@ -13,42 +13,127 @@ import core.signature as sig
 import click
 from tqdm import tqdm
 from intelhex import IntelHex
+import getpass
 
-@click.command(no_args_is_help=True)
+@click.group()
 @click.version_option(__version__, message="%(version)s")
-@click.option(
-    '-b', '--bootloader',
-    type=click.File('r'),
-    help='Intel HEX file containing Bootloader. '
-         'If not provided, an upgrade file will contain only the Firmware.',
-)
-@click.option(
-    '-f', '--firmware',
-    type=click.File('r'),
-    help='Intel HEX file containing Firmware. '
-         'If not provided, an upgrade file will contain only the Bootloader.',
-)
-@click.option(
-    '-s', '--sign',
-    type=click.File('rb'),
-    help='Sign with a private key provided in a PEM file. '
-         'This option may be used without HEX files to add a signature to '
-         'an existing upgrade file.',
-)
-@click.option(
-    '-p', '--passphrase',
-    help='Passphrase used to decrypt the private key. '
-         'Results in error if the private key is not encrypted.',
-)
-@click.option(
-    '-a', '--ask-passphrase', 'ask_passphrase',
-    flag_value=True, default=False,
-    help='Requests a passphrase when it is needed to decrypt the private key.'
-)
-def upgrade_generator(bootloader, firmware, sign, passphrase, ask_passphrase):
-    """Upgrade file generator"""
+#@click.command(no_args_is_help=True)
+def cli():
+    """Upgrade file generator."""
 
-    print("Processing...")
+@cli.command(
+    'gen',
+    short_help='generate a new upgrade file'
+)
+@click.option(
+    '-b', '--bootloader', 'bootloader_hex',
+    type=click.File('r'),
+    help='Intel HEX file containing Bootloader.',
+    metavar='<file.hex>'
+)
+@click.option(
+    '-f', '--firmware', 'firmware_hex',
+    type=click.File('r'),
+    help='Intel HEX file containing Firmware.',
+    metavar='<file.hex>'
+)
+@click.option(
+    '-k', '--private-key', 'key_pem',
+    type=click.File('rb'),
+    help='Private key in PEM container.',
+    metavar='<file.pem>'
+)
+@click.argument(
+    'upgrade_file',
+    required=True,
+    type=click.File('wb+'),
+    metavar='<upgrade_file.bin>'
+)
+def generate(upgrade_file, bootloader_hex, firmware_hex, key_pem):
+    """This command generates an upgrade file from given firmware files
+    in Intel HEX format. It is required to specify at least one firmware
+    file: Firmware or Bootloader.
+
+    In addition, if a private key is provided it is used to sign produced
+    upgrade file. Private key should be in PEM container with or without
+    encryption.
+    """
+
+    if not (bootloader_hex or firmware_hex):
+        raise click.ClickException("No input file specified")
+
+    # TODO: Create payload sections from HEX files
+    # TODO: Serialize sections to .bin file
+
+    # Sign created file if requested
+    if key_pem:
+        upgrade_file.seek(0)
+        sign(upgrade_file, key_pem)
+    else:
+        upgrade_file.close()
+
+@cli.command(
+    'sign',
+    short_help='sign existing upgrade file'
+)
+@click.option(
+    '-k', '--private-key', 'key_pem',
+    required=True,
+    type=click.File('rb'),
+    help='Private key in PEM container used to sign produced upgrade file.',
+    metavar='<filename.pem>'
+)
+@click.argument(
+    'upgrade_file',
+    required=True,
+    type=click.File('rb+'),
+    metavar='<upgrade_file.bin>'
+)
+def sign(upgrade_file, key_pem):
+    """This command adds a signature to an existing firmware file. Private key
+    should be provided in PEM container with or without encryption.
+
+    The signature is checked for duplication, and any duplicating signatures
+    are removed automatically.
+    """
+
+    # TODO: Load sections from .bin file
+    # TODO: Create signature section if absent
+    # TODO: Add signature if not existant yet with given key
+    # TODO: Serialize sections to .bin file
+    pass
+
+@cli.command(
+    'dump',
+    short_help='dump sections and signatures from upgrade file'
+)
+@click.argument(
+    'upgrade_file',
+    required=True,
+    type=click.File('rb'),
+    metavar='<upgrade_file.bin>'
+)
+def dump(upgrade_file):
+    """ This command dumps information regarding firmware sections and lists
+    signatures with public key fingerprints.
+    """
+
+    # TODO: Load sections from .bin file
+    # TODO: Dump information about payload sections
+    # TODO: Dump contents of signature section
+    pass
+
+def load_seckey(key_pem):
+    if not key_pem:
+        return None
+    data = key_pem.read()
+    if(sig.is_pem_encrypted(key_pem)):
+        password = getpass.getpass("Passphrase:")
+        return sig.seckey_from_pem(data, password)
+    return sig.seckey_from_pem(data)
+
+def create_payload_section(hex_file):
+    pass
 
 if __name__ == '__main__':
-    upgrade_generator()
+    cli()
