ae3a68ff584a9592cdeca3c7c0cd6e5b23e4a42d
Mike Tolkachev <mstolkachev@gmail.com>
2020-08-29 20:33:41 +0300

    Add error handlers for libsecp256k1

--- core/secp256k1_add/ext_callbacks.c
+++ core/secp256k1_add/ext_callbacks.c
@@ -1,8 +1,16 @@
-// TODO: handle errors
+/**
+ * @file       ext_callbacks.c
+ * @brief      External callbacks for libsecp256k1 library
+ * @author     Mike Tolkachev <contact@miketolkachev.dev>
+ * @copyright  Copyright 2020 Crypto Advance GmbH. All rights reserved.
+ */
+
+#include "bl_syscalls.h"
+
 void secp256k1_default_illegal_callback_fn(const char* str, void* data) {
-  // TODO call fatal error
+  blsys_fatal_error(str);
 }
 
 void secp256k1_default_error_callback_fn(const char* str, void* data) {
-  // TODO call fatal error
+  blsys_fatal_error(str);
 }
